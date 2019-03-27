37d5a1063a3286467847f6fea6647f1e63ab4203
Ryan Febriansyah <15523163@students.uii.ac.id>
2019-03-27 18:56:53 +0700

    write test models

--- mocklibrary/catalog/tests/test_models.py
+++ mocklibrary/catalog/tests/test_models.py
@@ -6,9 +6,48 @@ from django.test import TestCase
 class AuthorModelTest(TestCase):
 
     """
-    Test Author models
+    Test Author models Params =
+    :first name labels:
+    :last name labels:
+    :date of birth labels:
+    :date of death labels:
     """
 
     @classmethod
     def setUp(self):
         Author.objects.create(first_name='Jane', last_name='Doe')
+
+    def test_first_name_labels(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEquals(field_label, 'first name')
+
+    def test_last_name_labels(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('last_name').verbose_name
+        self.assertEquals(field_label, 'last name')
+
+    def test_date_of_birth_labels(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('date_of_birth').verbose_name
+        self.assertEquals(field_label, 'date of birth')
+
+    def test_date_of_death_labels(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('date_of_death').verbose_name
+        self.assertEquals(field_label, 'Died')
+
+    def test_first_name_max_length_fields(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('first_name').max_length
+        self.assertEquals(max_length, 150)
+
+    def test_last_name_max_length_fields(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('last_name').max_length
+        self.assertEquals(max_length, 150)
+
+    #this test will fail if url conf not setting in
+    #def test_get_absolute_url(self):
+    #    author = Author.objects.get(id=1)
+    #    self.assertEquals(author.get_absolute_url(), '/catalog/author/')
